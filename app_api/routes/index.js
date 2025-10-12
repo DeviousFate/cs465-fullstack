552a8e4a31cb56b9e8e88e5a70c149136ed28ee4
Thomas Cheek <cheekdylan13@gmail.com>
2025-10-12 18:59:22 -0500

    Module 6 WIP

--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -1,14 +1,26 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const tripsController = require('../controllers/trips');
+const tripsController = require("../controllers/trips");
+const mealsController = require("../controllers/meals");
+const newsController = require("../controllers/news");
+const roomsController = require("../controllers/rooms");
 
-router
-    .route('/trips')
-    .get(tripsController.tripsList);
+router //DEFINE ROUTE FOR TRIPS ENDPOINTS
+  .route("/trips")
+  .get(tripsController.tripsList)
+  .post(tripsController.addTrip);
 
-router
-    .route('/trips/:tripCode')
-    .get(tripsController.tripsFindByCode);
-    
+router //GET METHOD
+  .route("/trips/:code")
+  .get(tripsController.tripsByCode)
+  .put(tripsController.tripsUpdateTrip);
+
+router.route("/meals").get(mealsController.mealsList);
+router.route("/meals/:code").get(mealsController.mealsByCode);
+router.route("/news").get(newsController.newsList);
+router.route("/news/:code").get(newsController.newsByCode);
+router.route("/rooms").get(roomsController.roomsList);
+router.route("/rooms/:code").get(roomsController.roomsByCode);
+
 module.exports = router;
